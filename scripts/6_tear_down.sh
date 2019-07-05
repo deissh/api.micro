3e482f8f24da94bdb289a845fb2864aef37527da
Kirill Kudryavtsev <deissh@yandex.ru>
2019-07-05 19:37:59 +0300

    chore(scripts): add tear down

--- scripts/6_tear_down.sh
+++ scripts/6_tear_down.sh
@@ -5,4 +5,21 @@
 #version         :0.1
 #=====================================
 
-echo "ToDo this"
+# GCP params
+readonly PROJECT='anibe-2bcf3'
+readonly CLUSTER='anibe-cluster'
+readonly REGION='europe-north1'
+
+# Delete GKE cluster (time in foreground)
+yes | gcloud beta container clusters delete ${CLUSTER} --region ${REGION}
+
+# Confirm network resources are also deleted
+gcloud compute forwarding-rules list
+gcloud compute target-pools list
+gcloud compute firewall-rules list
+
+# In case target-pool associated with Cluster is not deleted
+yes | gcloud compute target-pools delete  \
+  $(gcloud compute target-pools list \
+    --filter="region:($REGION)" --project ${PROJECT} \
+  | awk 'NR==2 {print $1}')
